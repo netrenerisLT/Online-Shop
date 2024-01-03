1176ccaec87f613462da5a0352a1d281c2ecf87d
Jonas Kazlauskas <netreneris@gmail.com>
2024-01-03 12:11:08 +0200

    project start

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const authRoutes = require("./routes/auth.routes");
+const path = require("path");
+
+const app = express();
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+app.use(authRoutes);
+
+app.listen(3000);
---
+++ controllers/auth.controller.js
@@ -0,0 +1,7 @@
+function getSignup(req, res, next) {}
+function getLogin(req, res, next) {}
+
+module.exports = {
+  getSignup: getSignup,
+  getLogin: getLogin,
+};
