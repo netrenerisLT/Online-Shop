343227fe320f1e4c3048786da19c0dbd7fb178d4
Jonas Kazlauskas <netreneris@gmail.com>
2024-01-08 19:53:07 +0200

    added image upload

--- middlewares/image-upload.js
+++ middlewares/image-upload.js
@@ -1,1 +1,15 @@
+const uuid = require("uuid").v4;
 const multer = require("multer");
+
+const upload = multer({
+  storage: multer.diskStorage({
+    destination: "product-data/images",
+    filename: function (req, file, cb) {
+      cb(null, uuid() + "-" + file.originalname);
+    },
+  }),
+});
+
+const configuredMulterMiddleware = upload.single("image");
+
+module.exports = configuredMulterMiddleware;
--- routes/admin.routes.js
+++ routes/admin.routes.js
@@ -1,9 +1,16 @@
 const express = require("express");
 const adminController = require("../controllers/admin.controller");
+const configuredMulterMiddleware = require("../middlewares/image-upload");
 const router = express.Router();
 
 router.get("/products", adminController.getProducts);
 
 router.get("/products/new", adminController.getNewProduct);
+
+router.post(
+  "/products",
+  configuredMulterMiddleware,
+  adminController.createNewProduct
+);
 
 module.exports = router;
