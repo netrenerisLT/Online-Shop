2517bed000432e24b6bbb7ebebff8496426d82db
Jonas Kazlauskas <netreneris@gmail.com>
2024-01-08 21:20:42 +0200

    added product list css

--- models/product.model.js
+++ models/product.model.js
@@ -4,7 +4,7 @@ class Product {
   constructor(productData) {
     this.title = productData.title;
     this.summary = productData.summary;
-    this.price = productData.price;
+    this.price = +productData.price;
     this.description = productData.description;
     this.image = productData.image;
     this.imagePath = `product-data/images${productData.image}`;
