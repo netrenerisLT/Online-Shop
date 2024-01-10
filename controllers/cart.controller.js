c3e950cd78ace357b418ab7cde92ba3a518c863b
Jonas Kazlauskas <netreneris@gmail.com>
2024-01-10 21:51:49 +0200

    finished cart and updating DOM

--- controllers/cart.controller.js
+++ controllers/cart.controller.js
@@ -24,10 +24,11 @@ async function addCartItem(req, res, next) {
 function updateCartItem(req, res) {
   const cart = res.locals.cart;
 
-  const updatedItemData = cart.updateCartItem(
+  const updatedItemData = cart.updateItem(
     req.body.productId,
     req.body.quantity
   );
+  console.log(updatedItemData);
 
   req.session.cart = cart;
 
--- models/cart.model.js
+++ models/cart.model.js
@@ -35,15 +35,15 @@ class Cart {
         const cartItem = { ...item };
         const quantityChange = newQuantity - item.quantity;
         cartItem.quantity = newQuantity;
-        cartItem.totalPrice = newQuantity * product.price;
+        cartItem.totalPrice = newQuantity * item.product.price;
         this.items[i] = cartItem;
 
         this.totalQuantity = this.totalQuantity + quantityChange;
-        this.totalPrice += quantityChange * product.price;
+        this.totalPrice += quantityChange * item.product.price;
         return {
           updatedItemPrice: cartItem.totalPrice,
         };
-      } else if (item.product.id === productId && newQuantity < 0) {
+      } else if (item.product.id === productId && newQuantity <= 0) {
         this.items.splice(i, 1);
         this.totalQuantity = this.totalQuantity - item.quantity;
         this.totalPrice -= item.totalPrice;
